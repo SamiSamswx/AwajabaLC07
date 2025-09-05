37fd51d9aff9fe35c80bfe1927941cb2a44200ab
S A M I <sami.debbari.sio@gmail.com>
2025-09-05 17:58:21 +0200

    Create aRepas.java

---
+++ Repas.java
@@ -0,0 +1,14 @@
+}
+		public int getNumero {
+			return numero;
+		}
+		public void setNumero(int numero){
+			this.numero = numero
+		}
+		public LocalDate getDate(){
+			return date
+		}
+		public void setDate(LocalDate date){
+			this.date = date;
+		}
+	}
